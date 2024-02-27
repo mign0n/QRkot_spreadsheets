c3b12d65f50ff2429913a248877381e38461dc93
mign0n <mign0n.git@gmail.com>
2024-02-27 17:52:44 +0500

    refactor: Add table size check for Google API. Optimize codebase.

--- app/services/report.py
+++ app/services/report.py
@@ -1,18 +1,22 @@
+from copy import deepcopy
 from datetime import datetime
 
 from aiogoogle import Aiogoogle
 
+from app.api.validators import check_data_fits_table_grid
 from app.core.config import settings
 from app.models import CharityProject
 
 FORMAT = '%Y/%m/%d %H:%M:%S'
 LOCALE = 'ru_RU'
-SPREADSHEET_TITLE = 'Отчёт от {date}'
+TITLE = 'Отчёт от'
 SPREADSHEET_PERMISSIONS = {
     'type': 'user',
     'role': 'writer',
     'emailAddress': settings.email,
 }
+ROW_COUNT = 100
+COLUMN_COUNT = 11
 SHEETS = [
     {
         'properties': {
@@ -20,46 +24,48 @@ SHEETS = [
             'sheetId': 0,
             'title': 'Лист1',
             'gridProperties': {
-                'rowCount': 100,
-                'columnCount': 11,
+                'rowCount': ROW_COUNT,
+                'columnCount': COLUMN_COUNT,
             },
         },
     },
 ]
-DATA_RANGE = 'A1:C{rows_number}'
+
 TABLE_HEADER = [
     ['Топ проектов по скорости закрытия'],
     ['Название проекта', 'Время сбора', 'Описание'],
 ]
+SPREADSHEET_PROPERTIES = {
+    'properties': {
+        'title': TITLE,
+        'locale': LOCALE,
+    },
+    'sheets': SHEETS,
+}
 
 
-async def spreadsheets_create(wrapper_services: Aiogoogle) -> str:
-    return (
-        await wrapper_services.as_service_account(
-            (
-                await wrapper_services.discover('sheets', 'v4')
-            ).spreadsheets.create(
-                json={
-                    'properties': {
-                        'title': SPREADSHEET_TITLE.format(
-                            date=datetime.now().strftime(FORMAT)
-                        ),
-                        'locale': LOCALE,
-                    },
-                    'sheets': SHEETS,
-                },
-            ),
-        )
-    )['spreadsheetId']
+async def spreadsheets_create(
+    wrapper_services: Aiogoogle,
+    properties=deepcopy(SPREADSHEET_PROPERTIES),
+) -> tuple[str, str]:
+    properties['properties']['title'] += ' {}'.format(
+        datetime.now().strftime(FORMAT)
+    )
+    response = await wrapper_services.as_service_account(
+        (await wrapper_services.discover('sheets', 'v4')).spreadsheets.create(
+            json=properties
+        ),
+    )
+    return response['spreadsheetId'], response['spreadsheetUrl']
 
 
 async def set_user_permissions(
-    spreadsheetid: str,
+    spreadsheet_id: str,
     wrapper_services: Aiogoogle,
 ) -> None:
     await wrapper_services.as_service_account(
         (await wrapper_services.discover('drive', 'v3')).permissions.create(
-            fileId=spreadsheetid,
+            fileId=spreadsheet_id,
             json=SPREADSHEET_PERMISSIONS,
             fields='id',
         ),
@@ -67,25 +73,34 @@ async def set_user_permissions(
 
 
 async def spreadsheets_update_value(
-    spreadsheetid: str,
+    spreadsheet_id: str,
     projects: list[CharityProject],
     wrapper_services: Aiogoogle,
 ) -> None:
-    table_values = TABLE_HEADER
-    for project in projects:
-        table_values.append(
-            [
-                project.name,
-                str(project.close_date - project.create_date),
-                project.description,
-            ]
-        )
+    table_values = [
+        [TITLE, datetime.now().strftime(FORMAT)],
+        *TABLE_HEADER,
+        *[
+            list(
+                map(
+                    str,
+                    [
+                        project.name,
+                        project.close_date - project.create_date,
+                        project.description,
+                    ],
+                )
+            )
+            for project in projects
+        ],
+    ]
+    await check_data_fits_table_grid(table_values, ROW_COUNT, COLUMN_COUNT)
     await wrapper_services.as_service_account(
         (
             await wrapper_services.discover('sheets', 'v4')
         ).spreadsheets.values.update(
-            spreadsheetId=spreadsheetid,
-            range=DATA_RANGE.format(rows_number=len(table_values)),
+            spreadsheetId=spreadsheet_id,
+            range='R1C1:R{}C{}'.format(ROW_COUNT, COLUMN_COUNT),
             valueInputOption='USER_ENTERED',
             json={'majorDimension': 'ROWS', 'values': table_values},
         )
